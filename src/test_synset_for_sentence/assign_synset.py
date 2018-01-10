e14c8ac8128b6fb02793375b44cf8fd31ed3f0e6
componavt <andrew.krizhanovsky@gmail.com>
2018-01-10 16:06:58 +0300

    errors fixed in assign_synset export histogramm

--- src/test_synset_for_sentence/assign_synset.py
+++ src/test_synset_for_sentence/assign_synset.py
@@ -68,76 +68,65 @@ for sent in sentences:
 print "Right answers:", str(alg1_sum_1)+',', "wrong answers:", str(alg1_sum_0)
 
 
-epsFile = open('data/epsilon_out.csv','w')
-epsFile.write(u"Epsilon\tRight\tWrong\n")
-
 print "__________________EXPERIMENT 2________________"
-# eps, near_set_num, far_set_num, alien_degree
-#for eps in range(0.05,0.95,0.05):
+epsFile = open('data/alg2eps_hist.csv','w')
+epsFile.write(u"Epsilon\tCount\n")
+
+lemmas = {}
+for lemma in synsets:
+    lemmas[lemma] = {}
+    for eps in np.arange(0.0, 1.0, 0.01):
+        lemmas[lemma][eps] = {'expert_plus': 0, 'expert_minus' : 0}
+
+# eps, near_set_num, far_set_num, alien_degree   # for eps in range(0.05,0.95,0.05):
+#epsilons = [];
 for eps in np.arange(0.0, 1.0, 0.01):
+    #epsilons[eps] = {'alg2_expert_plus': 0, 'alg2_expert_minus' : 0}
     alg2_expert_plus  = 0
     alg2_expert_minus = 0
     for sent in sentences:
         # the list 'sentences' will be updated in the function below
         (a, b) = test_synset_for_sentence.lib_sfors.synset_selector.selectSynsetForSentenceByAlienDegree (
                                                                         sent, sentences, synsets, model, eps )
+        lemma = sentences[sent]['lemma']
+        lemmas[lemma][eps]['expert_plus']  += a
+        lemmas[lemma][eps]['expert_minus'] += b
         alg2_expert_plus  += a
         alg2_expert_minus += b
     
     print "Epsilon:", str(eps)+',', "right answers:", str(alg2_expert_plus)+',', "wrong answers:", str(alg2_expert_minus)
 
-    epsFile.write(u"{}\t{}\t{}\n".format(str(eps), str(alg2_expert_plus), str(alg2_expert_minus)))
+#    epsFile.write(u"{}\t{}\t{}\n".format(str(eps), str(alg2_expert_plus), str(alg2_expert_minus)))
+    epsFile.write(u"{}\t{}\n".format(str(eps), str(alg2_expert_plus)))
 epsFile.close()
-sys.exit("\nLet's stop and think.")
+
+lemmaFile = open('data/9lemma_hist.csv','w')
+for lemma in lemmas:
+    lemmaFile.write(lemma+"\n")
+    lemmaFile.write("\tEpsilon\tRight\tWrong\n")
+    for eps in np.arange(0.0, 1.0, 0.01):
+        lemmaFile.write(u"\t{}\t{}\t{}\n".format(str(eps), str(lemmas[lemma][eps]['expert_plus']), str(lemmas[lemma][eps]['expert_minus'])))
+lemmaFile.close()
+
+#sys.exit("\nLet's stop and think.")
 
 sentFile = open('data/sentence_out.csv','w')
-sentFile.write(u"Sentence\tLemma\tN lemmas\tExpert\tAlg1(average)\tAlg2(alien)\tAlg1+\tAlg2+\n")
+sentFile.write(u"Sentence\tLemma\tN lemmas\tExpert\tAlg1(average)\tAlg2(alien)\tAlg1+\tAlg2+\tAlg1+Alg2\n")
 
 for sent in sentences:
+    alg1_plus_alg2 = sentences[sent]['alg1_right'] + sentences[sent]['alg2_right']
 #    sentFile.write(sent + "\t" + str(sentences[sent]['synset_exp']) + "\t" + str(sentences[sent]['synset_alg1']) + "\t" + str(sentences[sent]['synset_alg2']) + "\n")
-    sentFile.write(u"{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n"
+    sentFile.write(u"{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n"
             .format(sent,sentences[sent]['lemma'],
                          str(len(sentences[sent]['lemmas'])),
                          str(sentences[sent]['synset_exp']),
                          str(sentences[sent]['synset_alg1']),
                          str(sentences[sent]['synset_alg2']),
                          str(sentences[sent]['alg1_right']),
-                         str(sentences[sent]['alg2_right'])))
+                         str(sentences[sent]['alg2_right']),
+                         str(alg1_plus_alg2)))
 sentFile.close()
 
-sys.exit("\nLet's stop and think.")
+#sys.exit("\nLet's stop and think.")
 
 
-line1 = [u'гам', u'гвалт', u'грохот', u'гул', u'устройство', u'странность']
-#line1 = u"гам гвалт грохот гул" # without "шум "
-line2 = [u'помеха', u'возмущение', u'роза', u'табуретка', u'мышь']
-#ine2 = u"помеха возмущение"    # without "шум "
-
-#gr1   = line1.split()
-#gr2   = line2.split()
-
-words1 = lib.filter_vocab_words.filterVocabWords( line1, model.vocab )
-print lib.string_util.joinUtf8( ",", words1 )                                # after filter, now there are only words with vectors
-
-#arr_vectors1 = []
-#for w in words1:
-#    # print u"    - '{}'".format( model[ w ] )
-#    arr_vectors1.append( model[ w ] )
-
-words2 = lib.filter_vocab_words.filterVocabWords( line2, model.vocab )
-print lib.string_util.joinUtf8( ",", words2 )                                # after filter, now there are only words with vectors
-
-#arr_vectors2 = []
-#for w in words2:
-#    # print u"    - '{}'".format( model[ w ] )
-#    arr_vectors2.append( model[ w ] )
-
-d = model.n_similarity(words1, words2)
-print u"d = {}".format( d )
-
-sys.exit("\nLet's stop and think.")
-
-print u"d = {} | gr1={} | gr2={}".format( d, test_word,  lib.string_util.joinUtf8( ",", gr1 ), 
-                                                         lib.string_util.joinUtf8( ",", gr2 ) )
-
-# sys.exit("\nLet's stop and think.")
